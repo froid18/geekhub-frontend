8a6ddacd6149a1103908e0f11ad1a7a6a16f24d3
Juan <273889@unifio.edu.br>
2026-09-04 00:48:41 +0000

    Adiciona FormsModule e exercicios 1 e 2 de data binding

--- geekhub/src/app/app.ts
+++ geekhub/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('geekhub');
+
+  nome = 'Carlos';
+  idade = 25;
+  curso = 'Sistemas de Informação';
+
+  produto = 'Teclado';
+  preco = 150;
+  quantidade = 3;
+
+  imagemProduto = 'https://via.placeholder.com/300';
+  descricaoImagem = 'Imagem de um produto';
+
 }
