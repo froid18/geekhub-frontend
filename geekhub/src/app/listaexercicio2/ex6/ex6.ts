41a6fe283863782c80caa7ff26fa9ca48cfcee7e
Juan <273889@unifio.edu.br>
2026-09-18 01:40:03 +0000

    ex 4 concluido

---
+++ geekhub/src/app/listaexercicio2/ex6/ex6.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex6',
+  standalone: false,
+  templateUrl: './ex6.html',
+  styleUrl: './ex6.scss',
+})
+export class Ex6 {
+
+  nomesIniciais = [
+    'Juan',
+    'Ana',
+    'Carlos',
+    'Gabriel',
+    'Mariana'
+  ];
+
+  nomes = [...this.nomesIniciais];
+
+  removerUltimo() {
+    this.nomes.pop();
+  }
+
+  limparLista() {
+    this.nomes = [];
+  }
+
+  restaurarLista() {
+    this.nomes = [...this.nomesIniciais];
+  }
+}
--- geekhub/src/app/listaexercicio2/listaexercicio2-routing-module.ts
+++ geekhub/src/app/listaexercicio2/listaexercicio2-routing-module.ts
@@ -5,7 +5,8 @@ import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
-
+import { Ex5 } from './ex5/ex5';
+import { Ex6 } from './ex6/ex6';
 const routes: Routes = [
   {
     path: 'ex1',
@@ -21,7 +22,12 @@ const routes: Routes = [
   },
   { path: 'ex4',
     component: Ex4
-   }
+   },
+  { path: 'ex5',
+    component: Ex5
+   },
+   { path: 'ex6',
+     component: Ex6 }
 ];
 
 @NgModule({
