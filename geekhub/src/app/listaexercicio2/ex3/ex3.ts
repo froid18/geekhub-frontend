9fdc060a8600422de49f90fa3281a089eb3c3513
Juan <273889@unifio.edu.br>
2026-09-18 01:04:33 +0000

    ex 2 concluido

---
+++ geekhub/src/app/listaexercicio2/ex3/ex3.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex3',
+  standalone: false,
+  templateUrl: './ex3.html',
+  styleUrl: './ex3.scss',
+})
+export class Ex3 {}
--- geekhub/src/app/listaexercicio2/listaexercicio2-module.ts
+++ geekhub/src/app/listaexercicio2/listaexercicio2-module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { Listaexercicio2RoutingModule } from './listaexercicio2-routing-module';
 import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
+import { Ex3 } from './ex3/ex3';
 
 @NgModule({
-  declarations: [Ex1],
+  declarations: [Ex1, Ex2, Ex3],
   imports: [CommonModule, Listaexercicio2RoutingModule],
 })
 export class Listaexercicio2Module {}
